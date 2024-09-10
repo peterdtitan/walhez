6b879e6285521ebed357c06fcbdffb08f7db4762
Peter Okorafor <peterdtitan@gmail.com>
2024-09-10 08:32:28 +0100

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+import {nextui} from "@nextui-org/react";
+
+
+const config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [nextui()],
+  darkMode: "class",
+}
+
+export default config;
